bb245dfe64a977c8d5398f988b49fb6ff1f8ad63
LeonardoAlbuquerque421 <leonardonascimento421@yahoo.com.br>
2023-06-15 18:25:44 -0300

    added readme

---
+++ cypress/integration/privacy.spec.js
@@ -0,0 +1,7 @@
+describe('Tela Politica de privacidade', function() {
+it('testa a página da política de privacidade de forma independente', function() {
+    cy.visit('./src/privacy.html')
+    
+    cy.contains('Talking About Testing').should('be.visible')
+    })
+})
